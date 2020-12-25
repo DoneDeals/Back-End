806bcaaa089e1ed99e832c3a8b725899c2375dc8
SuilarsoJapit <suilarso.japit@skipthedishes.ca>
2020-12-25 15:16:06 -0600

    Initial file for itemRoures

---
+++ routes/itemRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} from '../controllers/userController.js'
+
+import { protect } from '../middleware/authMiddleware.js'
+
+const router = express.Router();
+
+router.route('/').post(addItem);
+
+//router
+//  .route('/profile')
+//  .get(protect, getUserProfile)
+//  .put(protect, updateUserProfile)
+
+export default router
